3f2b2f7acdaabff1747e6493c4b493a05304e858
ibnu naufal <ibnunaufal5758@gmail.com>
2024-04-03 11:28:30 +0700

    change dir and module name

--- app/src/main/java/com/naufall/nfctooltki/MainActivity.kt
+++ app/src/main/java/com/naufall/nfctooltki/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.naufall.nfctooltki
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,5 +17,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+//        NfcTools.getValidNfcFromIntent(intent!!)
     }
 }
