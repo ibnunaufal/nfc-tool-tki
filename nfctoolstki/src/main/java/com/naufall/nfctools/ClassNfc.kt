43b8c0fc08e5011da22b85a659d82b536cc4d4de
ibnu naufal <ibnunaufal5758@gmail.com>
2024-08-01 09:27:34 +0700

    fix minor

--- nfctoolstki/src/main/java/com/naufall/nfctools/ClassNfc.kt
+++ nfctoolstki/src/main/java/com/naufall/nfctools/ClassNfc.kt
@@ -9,7 +9,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
-class ClassNfc: AppCompatActivity() {
+open class ClassNfc: AppCompatActivity() {
     private var adapter: NfcAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
